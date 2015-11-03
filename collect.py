61409077cd391b98300e9e14fb78f33b98cb67b5
Lorenzo Lancia <lorenzo@lor-f942.station>
2015-11-03 19:39:36 +0100

    Adding collect and gui structure

---
+++ collect.py
@@ -0,0 +1,50 @@
+import urllib2
+from bs4 import BeautifulSoup
+import os
+import time
+
+url = "http://www.kijiji.it/case/vendita/roma-annunci-roma/"
+a = urllib2.urlopen(url)
+soup = BeautifulSoup(a)
+npage =  soup.find("a", { "class" : "last-page" })
+npage=  int(npage.contents[0])
+
+#se non esiste la cartella documents allora creala
+if not os.path.exists("documents"):
+    os.makedirs("documents")
+root="./documents/"
+entry_id = 0
+#iterare su tutte le pagine di kijiji
+
+for kijpage in range(1,npage+1):
+    print "Scarico pagina", kijpage, " di ", npage
+    pagurl = url+"?p="+str(kijpage)
+    a = urllib2.urlopen(pagurl)
+    soup = BeautifulSoup(a)
+    topresult = soup.findAll("li", {"class": "item topad result"})
+    results = soup.findAll("li", {"class": "item result"})
+    results.extend(topresult)
+    #iterazione su tutti gli annunci di una pagina
+    for item in results:
+        if entry_id%500 ==0:
+            if not os.path.exists("documents/documents-"+str(entry_id).zfill(6)+"-"+str(entry_id+500).zfill(6)):
+                os.makedirs("documents/documents-"+str(entry_id).zfill(6)+"-"+str(entry_id+500).zfill(6))
+            path=root+"/documents-"+str(entry_id).zfill(6)+"-"+str(entry_id+500).zfill(6)+"/"
+        record_entry = open (path+str(entry_id).zfill(6)+".txt", "w")
+        title = item.h3.contents[0].encode("utf-8")
+        location = item.find("p", {"class": "locale"}).contents[0].encode("utf-8")
+        price = item.h4.contents[0].encode("utf-8")
+        adurl = item.find("a", {"class": "cta"})
+        adurl = adurl['href']
+        description = item.find("p", {"class": "description"}).contents[0].encode("utf-8")
+        record_entry.write(title+"\t"+location+"\t"+price+"\t"+adurl+"\t"+description)
+        #print  title+"\t"+location+"\t"+price+"\t"+adurl+"\t"+description+"\n"
+        #Chiudere il file
+        
+        entry_id +=1
+
+    time.sleep(0.1)
+    
+    
+
+
